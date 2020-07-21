6b53f8c65f2d0071c9c643439c141725e0245687
aubrin-s <68128717+aubrin-s@users.noreply.github.com>
2020-07-20 17:34:38 -0700

    Created using Colaboratory

---
+++ dummy.py
@@ -0,0 +1,55 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "dummy.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyPnpZKTVbcmtdisEBA/6tVd",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/aubrin-s/repository1/blob/master/dummy.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "bLpEO-9AxR-f",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 51
+        },
+        "outputId": "4c0addf9-4ce6-47f6-bd90-8dccb7e6b3c7"
+      },
+      "source": [
+        "print('hello')\n",
+        "print('its me')"
+      ],
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "hello\n",
+            "its me\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
